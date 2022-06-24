2e343d05c50dd91c90292cb806e841edaea0f3c2
xaviergeorge <13365640+xaviergeorge@users.noreply.github.com>
2022-06-24 13:43:16 +0200

    jwt and hash

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -5,12 +5,22 @@ const {
   loginUser,
   createUser,
   updateUser,
+  refreshTokenGen,
+  deleteUserTest,
+  verify,
+  logoutUser,
 } = require("../controllers/userController");
 
 router.post("/createUser", createUser);
 
 router.post("/loginUser", loginUser);
 
+router.post("/logout", verify, logoutUser);
+
 router.put("/updateUser", updateUser);
+
+router.post("/api/refreshtoken", refreshTokenGen);
+
+router.delete("/api/delete", verify, deleteUserTest);
 
 module.exports = router;
