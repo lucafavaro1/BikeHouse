52cc2377965a263dc1d5d660c34a96c818aa3285
Tarlan Ismayilsoy <ge47qat@mytum.de>
2022-07-15 14:58:18 +0200

    added buy button to accessories

--- frontend/src/components/pages/AccessoryDescription.js
+++ frontend/src/components/pages/AccessoryDescription.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "../css/AccessoryDescription.css"
-import { Row } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 
 function AccessoryDescription(props) {
 	return (
@@ -11,8 +13,14 @@ function AccessoryDescription(props) {
 			<Row >
 				<span className="accName">{props.accessory.name}</span>
 			</Row>
-			<Row >
-				<span className="accPrice">&euro; {props.accessory.price}</span>
+			<Row className="mt-2">
+				<Col className="p-0">
+					<span className="accPrice">&euro; {props.accessory.price}</span>
+				</Col>
+
+				<Col className="p-0">
+					<button className="btn float-right addToCartBtn"><FontAwesomeIcon icon={faCartPlus} size="xl" /></button>
+				</Col>
 			</Row>
 		</div>
 	);
